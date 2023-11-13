0a3cf19c9bcb6f4339c8d758d4c90b5965172bbd
Hayoon <hy97@sju.ac.kr>
2023-11-13 22:20:52 +0900

    feat(EventManager): 할인정책별 할인금액 산정을 위한 HashMap<String, Integer> 구현

--- src/main/java/christmas/model/domain/EventManager.java
+++ src/main/java/christmas/model/domain/EventManager.java
@@ -2,11 +2,17 @@ package christmas.model.domain;
 
 import christmas.model.domain.menu.Menu;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class EventManager {
     private List<Menu> orderMenuList;
     private int totalPrice;
+    private HashMap<String, Integer> benefitDetails;
+
+    public EventManager() {
+        initializeBenefitDetails();
+    }
 
     public void orderMenuList(List<Menu> orderMenuList) {
         this.orderMenuList = orderMenuList;
@@ -16,6 +22,14 @@ public class EventManager {
         for (Menu menu : this.orderMenuList) {
             this.totalPrice += menu.getPrice();
         }
+    }
+
+    public void initializeBenefitDetails() {
+        benefitDetails.put("크리스마스 디데이 할인", 0);
+        benefitDetails.put("평일 할인", 0);
+        benefitDetails.put("주말 할인", 0);
+        benefitDetails.put("특별 할인", 0);
+        benefitDetails.put("증정 이벤트", 0);
     }
 
     public boolean isEligibleForDiscount() {
@@ -34,4 +48,7 @@ public class EventManager {
         return totalPrice;
     }
 
+    public HashMap<String, Integer> getBenefitDetails() {
+        return benefitDetails;
+    }
 }
