5d05576b6e86f719a7d58a908611734ab688973e
Hayoon <hy97@sju.ac.kr>
2023-11-14 01:34:35 +0900

    feat(EventBadge): 이벤트 뱃지 Enum Type 구현

---
+++ src/main/java/christmas/model/domain/EventBadge.java
@@ -0,0 +1,36 @@
+package christmas.model.domain;
+
+public enum EventBadge {
+
+    SANTA("산타", 20000),
+    TREE("트리", 10000),
+    STAR("별", 5000),
+    NONE("없음", 0);
+
+    private final String category;
+    private final int minAmount;
+
+    EventBadge(String category, int minAmount) {
+        this.category = category;
+        this.minAmount = minAmount;
+    }
+
+    public static EventBadge getEventBadge(int minAmount) {
+        if (minAmount >= SANTA.minAmount) {
+            return EventBadge.SANTA;
+        } else if (minAmount >= TREE.minAmount) {
+            return EventBadge.TREE;
+        } else if (minAmount >= STAR.minAmount) {
+            return EventBadge.STAR;
+        }
+        return NONE;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getMinAmount() {
+        return minAmount;
+    }
+}
