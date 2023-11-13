82a5a5006597af343585d85769aeac88d763c4e7
Hayoon <hy97@sju.ac.kr>
2023-11-13 21:37:04 +0900

    feat(DDayPolicy): 크리스마스 디데이 이벤트 할인 discount() 오버라이딩 메서드 구현

---
+++ src/main/java/christmas/model/domain/discount/policy/DDayPolicy.java
@@ -0,0 +1,23 @@
+package christmas.model.domain.discount.policy;
+
+import christmas.model.domain.EventManager;
+import christmas.model.domain.discount.DiscountPolicyName;
+
+import java.util.HashMap;
+
+import static christmas.model.domain.discount.DiscountPolicyName.*;
+
+public class DDayPolicy implements DiscountPolicy {
+    @Override
+    public void discount(EventManager eventManager, int date) {
+        HashMap<String, Integer> benefitDetails = eventManager.getBenefitDetails();
+        benefitDetails.putIfAbsent(CHRISTMAS_D_DAY.getDiscountPolicy(),
+                benefitDetails.getOrDefault(CHRISTMAS_D_DAY.getDiscountPolicy(), 0) + discountAlgorithm(date));
+    }
+
+    public int discountAlgorithm(int day) {
+        int money = 1000;
+        return (day - 1) * 100 + money;
+    }
+}
+
