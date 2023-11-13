4f3308836eb7854b54e326dcd68246396da79780
Hayoon <hy97@sju.ac.kr>
2023-11-13 20:16:51 +0900

    feat(InputView): readMenu() 주문할 메뉴 이름과 개수를 입력한다

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,31 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.util.validate.InputValidate;
+
+public class InputView {
+
+    public int readDate() {
+        while(true) {
+            try {
+                System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+                String input = Console.readLine();
+                int integerInput = InputValidate.parseAndValidateIntegerInput(input);
+                return integerInput;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public String readMenu() {
+        while(true) {
+            try {
+                System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+                return Console.readLine();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
