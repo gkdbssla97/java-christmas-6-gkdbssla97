40c6956fe0117bcbea8f9d134ae309745daccdc8
Hayoon <hy97@sju.ac.kr>
2023-11-13 22:33:08 +0900

    refactor(EventManagerService): 할인혜택 목록 적용 메서드 메서드 추출로 분리

--- src/main/java/christmas/model/service/EventManagerService.java
+++ src/main/java/christmas/model/service/EventManagerService.java
@@ -2,30 +2,56 @@ package christmas.model.service;
 
 import christmas.model.domain.EventCalendar;
 import christmas.model.domain.EventManager;
-import christmas.model.domain.discount.condition.DDayCondition;
-import christmas.model.domain.discount.condition.DiscountCondition;
-import christmas.model.domain.discount.policy.DDayPolicy;
-import christmas.model.domain.discount.policy.DiscountPolicy;
+import christmas.model.domain.discount.condition.*;
+import christmas.model.domain.discount.policy.*;
 
 public class EventManagerService {
-    private DiscountPolicy discountPolicy;
 
+    public void processBenefitDetails(int date, EventManager eventManager, EventCalendar eventCalendar) {
 
-    /**
-     * 크리스마스 디데이 할인: -1,200원
-     * 평일 할인: -4,046원
-     * 특별 할인: -1,000원
-     * 증정 이벤트: -25,000원
-     * @param eventManager
-     * @param eventCalendar
-     */
-    public void calculateBenefitDetails(int date, EventManager eventManager, EventCalendar eventCalendar,
-                                        DiscountPolicy discountPolicy, DiscountCondition discountCondition) {
-        discountCondition = new DDayCondition();
-        discountPolicy = new DDayPolicy();
+        applyDDayBenefit(date, eventManager, eventCalendar);
+        applyWeekdayBenefit(date, eventManager, eventCalendar);
+        applyWeekendBenefit(date, eventManager, eventCalendar);
+        applySpecialDayBenefit(date, eventManager, eventCalendar);
+        applyPresentEventBenefit(date, eventManager);
+    }
+
+    private void applyDDayBenefit(int date, EventManager eventManager, EventCalendar eventCalendar) {
+        DiscountCondition discountCondition = new DDayCondition();
+        DiscountPolicy discountPolicy = new DDayPolicy();
         if(discountCondition.isSatisfied(eventCalendar, date)) {
             discountPolicy.discount(eventManager, date);
         }
+    }
 
+    private void applyWeekdayBenefit(int date, EventManager eventManager, EventCalendar eventCalendar) {
+        DiscountCondition discountCondition = new WeekdayCondition();
+        DiscountPolicy discountPolicy = new WeekdayPolicy();
+        if(discountCondition.isSatisfied(eventCalendar, date)) {
+            discountPolicy.discount(eventManager, date);
+        }
+    }
+
+    private void applyWeekendBenefit(int date, EventManager eventManager, EventCalendar eventCalendar) {
+        DiscountCondition discountCondition = new WeekendCondition();
+        DiscountPolicy discountPolicy = new WeekendPolicy();
+        if(discountCondition.isSatisfied(eventCalendar, date)) {
+            discountPolicy.discount(eventManager, date);
+        }
+    }
+
+    private void applySpecialDayBenefit(int date, EventManager eventManager, EventCalendar eventCalendar) {
+        DiscountCondition discountCondition = new SpecialCondition();
+        DiscountPolicy discountPolicy = new SpecialPolicy();
+        if(discountCondition.isSatisfied(eventCalendar, date)) {
+            discountPolicy.discount(eventManager, date);
+        }
+    }
+
+    private void applyPresentEventBenefit(int date, EventManager eventManager) {
+        DiscountPolicy discountPolicy = new PresentEventPolicy();
+        if(eventManager.isEligibleForPresentMenu()) {
+            discountPolicy.discount(eventManager, date);
+        }
     }
 }
