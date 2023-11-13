396b999bc52b82f9e2ef192697fa599d1de0ef95
Hayoon <hy97@sju.ac.kr>
2023-11-13 19:26:05 +0900

    feat(MenuService): 주문 메뉴 카테고리 검색(findMenuCategory) 메서드 구현

--- src/main/java/christmas/model/service/MenuService.java
+++ src/main/java/christmas/model/service/MenuService.java
@@ -1,6 +1,6 @@
 package christmas.model.service;
 
-import christmas.model.domain.Category;
+import christmas.model.domain.EventManager;
 import christmas.model.domain.menu.*;
 
 import java.util.ArrayList;
@@ -8,20 +8,26 @@ import java.util.List;
 
 public class MenuService {
 
-    /**
-     * <애피타이저>
-     * 양송이수프(6,000), 타파스(5,500), 시저샐러드(8,000)
-     *
-     * <메인>
-     * 티본스테이크(55,000), 바비큐립(54,000), 해산물파스타(35,000), 크리스마스파스타(25,000)
-     *
-     * <디저트>
-     * 초코케이크(15,000), 아이스크림(5,000)
-     *
-     * <음료>
-     * 제로콜라(3,000), 레드와인(60,000), 샴페인(25,000)
-     */
-    public void initMenuList() {
+    public void orderMenuList(EventManager eventManager, String menus, List<Menu> menuList) {
+        List<Menu> orderMenuList = new ArrayList<>();
+        String[] menusInfos = menus.split(",");
+        for(String menuInfo : menusInfos) {
+            String[] menu = menuInfo.split("-");
+            String name = menu[0];
+            String price = menu[1];
+            Category menuCategory = findMenuCategory(menuList, name);
+        }
+    }
+
+    public Category findMenuCategory(List<Menu> menuList, String name) {
+        for(Menu menu : menuList) {
+            if(menu.getName().equals(name)) {
+                return menu.getCategory();
+            }
+        } throw new IllegalArgumentException();
+    }
+
+    public List<Menu> initMenuList() {
         List<Menu> menuList = new ArrayList<>();
 
         registerAppetizer(menuList);
@@ -29,6 +35,7 @@ public class MenuService {
         registerDessert(menuList);
         registerDrink(menuList);
 
+        return menuList;
     }
 
     private void registerDrink(List<Menu> menuList) {
