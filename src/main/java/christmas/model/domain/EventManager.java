39a6e5d0a87551ccbcad206ac8c1a443572ad082
Hayoon <hy97@sju.ac.kr>
2023-11-14 01:37:40 +0900

    feat(EventManager): EventBadge 필드변수 선언 및 혜택 금액에 따른 뱃지 부여

--- src/main/java/christmas/model/domain/EventManager.java
+++ src/main/java/christmas/model/domain/EventManager.java
@@ -4,12 +4,16 @@ import christmas.model.domain.discount.DiscountPolicyName;
 import christmas.model.domain.menu.Menu;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EventManager {
     private List<Menu> orderMenuList;
     private int totalPrice;
-    private HashMap<DiscountPolicyName, Integer> benefitDetails;
+    private final HashMap<DiscountPolicyName, Integer> benefitDetails = new LinkedHashMap<>();
+    private EventBadge eventBadge;
+    private int benefitPrice;
 
     public EventManager() {
         initializeBenefitDetails();
@@ -42,11 +46,41 @@ public class EventManager {
     }
 
     public List<Menu> getOrderMenuList() {
-        return this.orderMenuList;
+        return orderMenuList;
+    }
+
+    public int getTotalPriceBeforeDiscount() {
+        return totalPrice;
     }
 
     public int getTotalPrice() {
         return totalPrice;
+    }
+
+    public String getEventBadge() {
+        return eventBadge.getCategory();
+    }
+
+    public int getBenefitPrice() {
+        return benefitPrice;
+    }
+
+    public int getTotalBenefitAfterDiscount() {
+        int totalBenefitByDiscount = this.totalPrice;
+        for(Map.Entry<DiscountPolicyName, Integer> benefitDetail : benefitDetails.entrySet()) {
+            totalBenefitByDiscount -= benefitDetail.getValue();
+        }
+        this.benefitPrice = totalBenefitByDiscount;
+
+        return totalBenefitByDiscount;
+    }
+
+    public int getTotalPriceAfterDiscount() {
+        return this.totalPrice - this.benefitPrice;
+    }
+
+    public void grantEventBadge() {
+        this.eventBadge = EventBadge.grantEventBadge(benefitPrice);
     }
 
     public HashMap<DiscountPolicyName, Integer> getBenefitDetails() {
