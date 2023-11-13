7dd18a1c717740bcce2d4139ab3c06ebcfd2bd0e
Hayoon <hy97@sju.ac.kr>
2023-11-14 02:55:19 +0900

    feat(InputView): 이벤트 안내문 출력 선언

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,6 +6,7 @@ import christmas.util.validate.InputValidate;
 public class InputView {
 
     public int readDate() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         while(true) {
             try {
                 System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
