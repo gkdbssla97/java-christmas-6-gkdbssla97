17cd1febefa036955f84e74a455f629dabaef6cf
Hayoon <hy97@sju.ac.kr>
2023-11-15 01:22:25 +0900

    feat(NumberConstant): 날짜, 이벤트 금액 int -> 상수처리

---
+++ src/main/java/christmas/util/constant/NumberConstant.java
@@ -0,0 +1,23 @@
+package christmas.util.constant;
+
+public class NumberConstant {
+
+    // 날짜
+    public static final int EVENT_YEAR = 2023;
+    public static final int CHRISTMAS_DAY = 25;
+    public static final int EVENT_BEGIN_DAY = 1;
+    public static final int EVENT_END_DAY = 31;
+
+    // 이벤트 할인가
+    public static final int INITIAL_DISCOUNT_MONEY = 1000;
+    public static final int DISCOUNT_PER_DAY = 100;
+    public static final int SPECIAL_DISCOUNT = -1000;
+    public static final int PRESENT_DISCOUNT = -25000;
+
+    public static final int ELIGIBLE_FOR_DISCOUNT = 10000;
+    public static final int ELIGIBLE_PRESENT_PRICE = 120000;
+
+    // 디데이 이벤트
+    public static final int D_DAY_MIN = 1;
+    public static final int D_DAY_MAX = 25;
+}
