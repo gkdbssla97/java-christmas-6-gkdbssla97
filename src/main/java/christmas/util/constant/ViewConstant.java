a39494b3f041d93d18c2645f94462ad6de424885
Hayoon <hy97@sju.ac.kr>
2023-11-15 01:21:38 +0900

    feat(ViewConstant): input/output View String -> 상수처리

---
+++ src/main/java/christmas/util/constant/ViewConstant.java
@@ -0,0 +1,32 @@
+package christmas.util.constant;
+
+public class ViewConstant {
+
+    // input
+    public static final String WELCOME_EVENT_PLANNER = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String INPUT_VISITING_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String INPUT_ORDERING_MENUS = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
+    // output
+    public static final String PREVIEW_EVENT_BENEFIT = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    public static final String ORDER_MENU = "<주문 메뉴>";
+    public static final String TOTAL_PRICE_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
+    public static final String PRESENT_MENU = "<증정 메뉴>";
+    public static final String BENEFIT_LIST = "<혜택 내역>";
+    public static final String TOTAL_BENEFIT_PRICE = "<총혜택 금액>";
+    public static final String TOTAL_PRICE_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
+    public static final String EVENT_BADGE = "<12월 이벤트 배지>";
+
+    public static final String CHAMPAGNE_ONE = "샴페인 1개";
+
+    // format
+    public static final String MENU_INFO_FORMAT = "%s %d개\n";
+    public static final String TOTAL_PRICE_BEFORE_DISCOUNT_FORMAT = "%,d원\n";
+    public static final String BENEFIT_LIST_FORMAT = "%s: %,d원\n";
+    public static final String TOTAL_BENEFIT_PRICE_FORMAT = "%,d원\n";
+    public static final String TOTAL_PRICE_AFTER_DISCOUNT_FORMAT = "%,d원\n";
+
+    public static final String NONE = "없음";
+
+
+}
